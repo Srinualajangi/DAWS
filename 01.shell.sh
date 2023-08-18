af21c9304e99745d8f44c1a58065490e12a36182
Srinu Alajangi --CNTR <Srinu_Alajangi@contractor.amat.com>
2023-08-18 21:31:50 +0530

    modified

--- 01.shell.sh
+++ 01.shell.sh
@@ -1,12 +1,16 @@
-#!/in/bash/
+#!/bin/bash/
+
 if [ $# -ne 1 ]; then 
     echo "Please specify the filename: $0 "
     exit 1
 fi
+
 filename="$1"
 
-if [ ! -f "$filename" ] then
+if [ ! -f "$filename" ]; then
     echo "File '$filename' not found"
     exit 1
 fi
+
+
 cat "$filename" | tr -s '[:space:]' '\n' | tr '[:upper:]' '[:lower:]' | tr -d '[:punct:]' | sort | uniq -c | sort -nr
