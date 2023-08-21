6929a7ee3a14a59140d61fdc775cfc6b83f9a91b
Srinu Alajangi --CNTR <Srinu_Alajangi@contractor.amat.com>
2023-08-21 13:21:25 +0530

    Added mail script

--- 04.shell.sh
+++ 04.shell.sh
@@ -14,11 +14,8 @@ MEM_USAGE=$(free -m | grep  Mem | awk '{print $3}')
 MEM_THRESHOLD=135
 message=""
 
-while
-do 
     if [$MEM_USAGE -gt $MEM_THRESHOLD ]; then
     message+="SYSTEM IS RUNNING WITH HIGH MEMORY UTILIZATION: $MEM_USAGE MB"
     fi
-done 
 
 echo -e "message: $message"
